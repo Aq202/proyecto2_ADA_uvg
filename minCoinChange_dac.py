32747ddb20b8e53ef5db3e71671416eaf3e55c45
Aq202 <diegoguatedb2002@gmail.com>
2024-03-24 00:24:18 -0600

    Implementación min coin change en dac

---
+++ minCoinChange_dac.py
@@ -0,0 +1,17 @@
+import sys
+def minCoinChange (coin, m, K):
+    if K == 0:
+        return 0
+    minCount = sys.maxsize
+    for i in range (m) :
+        if coin[i] <= K:
+            tempResult = 1 + minCoinChange (coin, m, K - coin[i])
+            if tempResult < minCount:
+                minCount = tempResult + 1
+    
+    if minCount == sys.maxsize:
+        return -1
+    return minCount
+    
+
+print(minCoinChange([1,5,10], 3, 30))
